446669755376300b02942c8a45b3de207c9790c8
Thomas Tuegel <ttuegel@mailbox.org>
2019-08-09 16:34:38 -0500

    Update build system

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((haskell-mode
+  (dante-repl-command-line . ("cabal" "v2-repl"))))
--- scratch.nix
+++ scratch.nix
@@ -1,12 +1,17 @@
-{ mkDerivation, base, Chart, Chart-diagrams, colour, diagrams-lib
-, diagrams-svg, lens, stdenv
+{ mkDerivation, base, Chart, Chart-diagrams, colour, containers
+, diagrams-lib, diagrams-svg, hpack, lens, recursion-schemes
+, stdenv
 }:
 mkDerivation {
   pname = "scratch";
   version = "0";
   src = ./.;
   libraryHaskellDepends = [
-    base Chart Chart-diagrams colour diagrams-lib diagrams-svg lens
+    base Chart Chart-diagrams colour containers diagrams-lib
+    diagrams-svg lens recursion-schemes
   ];
-  license = stdenv.lib.licenses.unfree;
+  libraryToolDepends = [ hpack ];
+  prePatch = "hpack";
+  license = "unknown";
+  hydraPlatforms = stdenv.lib.platforms.none;
 }
